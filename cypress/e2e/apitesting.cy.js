10394841a1dea2ef424909a8e871e1ff0e31f239
malakashraf15 <152813883+malakashraf15@users.noreply.github.com>
2026-05-07 13:56:30 +0300

    changes

--- cypress/e2e/apitesting.cy.js
+++ cypress/e2e/apitesting.cy.js
@@ -10,17 +10,7 @@ it('add item', () => {
     cy.intercept('GET', '**/products', { fixture: 'products.json' })
     cy.intercept('GET', '**/categories*', { fixture: 'categories.json' })
     
-
 })
 
 
-it.skip('modify api response', () => {
-    cy.intercept('GET', '**/categories*', req => {
-        req.continue( res => {
-            res.body.articles[0].favoritesCount = 9999999
-            res.send(res.body)
-        })
-    })
-    cy.loginToApplication()
-    cy.get('app-favorite-button').first().should('contain.text', '9999999')
-})
+
