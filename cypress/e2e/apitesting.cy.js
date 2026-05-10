12ae69f5ff3d44e3e5edc722b3063bbf8e611efb
malakashraf15 <152813883+malakashraf15@users.noreply.github.com>
2026-05-10 15:01:11 +0300

    api mocking

--- cypress/e2e/apitesting.cy.js
+++ cypress/e2e/apitesting.cy.js
@@ -2,15 +2,39 @@
 
 //import HomePage from '../pages/HomePage'
 
-it('add item', () => {
+it('first mock [delete categories ]', () => {
+
+    cy.intercept('GET', '**/products', { fixture: 'products.json' })
+    cy.intercept('GET', '**/categories*', { fixture: 'categories.json' })
+    cy.visit('/')
+    cy.contains('Products').click()
+    
+})
+it('second mock [item soldout]', () => {
+    cy.intercept('GET', '**/user/api/products', { fixture: 'soldout-product.json' }).as('getProducts')
+    cy.intercept('GET', '**/admin/api/categories*', { fixture: 'categories.json' }).as('getCategories')
+
 
     cy.visit('/')
     cy.contains('Products').click()
 
-    cy.intercept('GET', '**/products', { fixture: 'products.json' })
-    cy.intercept('GET', '**/categories*', { fixture: 'categories.json' })
-    
+    cy.wait('@getProducts')
+    cy.contains('sold out').should('be.visible')
 })
+
+it('third mock - empty products list', () => {
+    cy.intercept('GET', '**/user/api/products', { fixture: 'empty-products.json' }).as('getProducts')
+    cy.intercept('GET', '**/admin/api/categories*', { fixture: 'categories.json' }).as('getCategories')
+
+    cy.visit('/')
+    cy.wait('@getProducts')
+
+    cy.contains('Products').click()
+
+    cy.get('.product-card-hover').should('not.exist')
+})
+
+
 
 
 
